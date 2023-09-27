f59ba269b0aa7a10ce4006f5ecc0a9d429457855
Pokotyliuk Oleh <olehpokotyliuk@gmail.com>
2023-09-27 05:17:58 +0300

    redeploy and fix link

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Result({correct}) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-      <a href='/'>Попробовать снова</a>
+      <a href='https://olehpokotyliuk.github.io/react-quiz/'>Попробовать снова</a>
     </div>
   );
 }
